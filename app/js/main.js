9af023d3b6a961d10d944c002e0b3538b0887f0c
Datosh <datoshcode@gmail.com>
2022-12-06 20:27:28 +0200

    added star rating

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
+// swiper
 var swiper = new Swiper(".comments__slider", {
   slidesPerView: "auto",
   spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
