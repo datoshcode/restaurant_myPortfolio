24869bd8af75e08694287f024ac7f31330fc8263
Datosh <datoshcode@gmail.com>
2022-12-06 18:38:13 +0200

    added 'comments clients'

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function () {
-  
-})
+var swiper = new Swiper(".comments__slider", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
